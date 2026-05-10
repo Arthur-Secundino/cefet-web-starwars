ac143a6e82998b8155b1ce92fd9e730f6f8b4e35
Arthur-Secundino <artsecundino@gmail.com>
2026-05-09 22:15:57 -0300

    Exercício 1 feito

--- starwars.js
+++ starwars.js
@@ -4,8 +4,19 @@
 //  - Pegar a lista de filmes (AJAX) e preencher no HTML
 //  - Quando um filme for clicado, exibir sua introdução
 
+import { play } from "./music.js";
+
 const API_ENDPOINT = 'https://swapi.info/api'
 
 const resposta = await fetch(API_ENDPOINT + "/films");
 const jsonFilmes = await resposta.json();
 console.log(jsonFilmes);
+
+const objetoMusica = {
+    audioUrl: "./audio/tema-sw.mp3",
+    coverImageUrl: "./imgs/logo.svg",
+    title: "Intro",
+    artist: "John Williams"
+};
+
+play(objetoMusica, document.body);
